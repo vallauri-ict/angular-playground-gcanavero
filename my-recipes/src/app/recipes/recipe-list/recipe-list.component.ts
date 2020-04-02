8edc30ba97db3e23b1b183e6bcfe87a22b9a50f6
gcanavero0417 <40639958+DSJohNNy01@users.noreply.github.com>
2020-04-02 12:11:27 +0200

    Lezione 2 aprile

--- my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Recipe} from "../Recipe.model";
+
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes:Recipe[]=[
+    new Recipe("A test recipe","Simply a test","https://i.imgur.com/fMdnwbd.jpg"),
+    new Recipe("Second test recipe","Simply a test","https://i.imgur.com/fMdnwbd.jpg"),
+    new Recipe("Third test recipe","Simply a test","https://i.imgur.com/fMdnwbd.jpg")
+  ]
 
   constructor() { }
 
